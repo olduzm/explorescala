677ad1e2b383dd830406e9a6113fb04cf4d476a8
mehmet olduz <olduzm@gmail.com>
2011-10-26 23:57:51 +0100

    progress on 25 lunchtime

--- onp.scala
+++ onp.scala
@@ -1,17 +1,17 @@
 
-
 type MyStr = List[Char]
 
 sealed abstract class Result[A]
 case class Success[A](c:A, remainder:MyStr) extends Result[A]
 case class Failure[A](message:String, remainder:MyStr) extends Result[A]
 
+type Parser[A] = (MyStr) => Result[A]
 
-def char(target:Char, input:MyStr) : Result[Char] =
+def char(target:Char) : Parser[Char] = (input =>
   input match {
     case c::cs if c==target => Success(c, cs)
     case _ => Failure("Next char is not " + target, input)
-  }
+  })
 
 def oneOf(targets:List[Char], input:MyStr) : Result[Char] =
   input match {
@@ -33,16 +33,38 @@ def number(input:MyStr) : Result[Int] =
 def word(input:MyStr) : Result[Char] =
   alphaChar(input)
 
-def then[A, B](first: (MyStr) => Result[A], second: (MyStr) => Result[B]): (MyStr) => Result[B] =
-  new Function1[MyStr, Result[B]] {
-    def apply(input: MyStr) = {
-      first(input) match {
-        case Success(c, t) => second(t)
-        case Failure(e, rem) => Failure(e, rem)
-      }
-    }
-  }
 
+def thenLeft[A, B](first: Parser[A], second: Parser[B]): Parser[A] = (input =>
+      first(input) match {
+        case Failure(e, rem) => Failure(e, rem)
+        case Success(v1, rem) => second(rem) match {
+          case Failure(e2, rem2) => Failure(e2, rem2)
+          case Success(_, rem2) => Success(v1, rem2)
+        }
+      })
+
+def then[A, B](first: Parser[A], second: Parser[B]): Parser[(A,B)] = (input =>
+      first(input) match {
+        case Failure(e, rem) => Failure(e, rem)
+        case Success(v1, rem) => second(rem) match {
+          case Failure(e, rem2) => Failure(e, rem2)
+          case Success(v2, rem2) => Success((v1, v2), rem2)
+        }
+      })
+
+case class Asgnmt(k:Char, v:Int)
+def pAssignment : Parser[Asgnmt] = (input =>
+    then(thenLeft(alphaChar, char('=')), number)(input) match {
+      case Success((k, v), rem) => Success(Asgnmt(k, v), rem)
+      case Failure(e, rem) => Failure(e, rem)
+    }
+  )
+
+then(then(alphaChar, digitChar), alphaChar) ("fa1l".toList)
+then(then(alphaChar, digitChar), alphaChar) ("s7cs".toList)
+pAssignment("a=7".toList)
+pAssignment("ab7".toList)
+pAssignment("a=".toList)
 
 type Superint = Int
 class Helper (left: Superint) {
